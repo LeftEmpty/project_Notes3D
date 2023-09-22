b7a42457c4dedcb8d6badf33396b81ed2f8ef4a5
LeftEmpty <43254602+LeftEmpty@users.noreply.github.com>
2023-09-22 19:38:23 +0200

    design overhaul
    changes:
    - redesign of index page
    - redesign of profile page
    - adjustments to uploads page
    
    todo:
    - media querries
    - write content for index page
    - backend functionality for profile data and contacts

--- public/js/uploads.js
+++ public/js/uploads.js
@@ -92,3 +92,19 @@ editForm.addEventListener("submit", (event) => {
         location.reload();
     })
 })
+
+const editBtn = document.querySelector('#btn--cur-note--edit');
+editBtn.addEventListener("click", toggleReadOnly);
+
+function toggleReadOnly() {
+    var textarea = document.querySelector("#cur-upload--note");
+    if (textarea.readOnly) {
+        textarea.classList.remove('cur-note--readonly');
+        textarea.classList.add('cur-note--editable');
+        textarea.removeAttribute("readonly");
+    } else {
+        textarea.classList.remove('cur-note--editable');
+        textarea.classList.add('cur-note--readonly');
+        textarea.setAttribute("readonly", "true");
+    }
+}
